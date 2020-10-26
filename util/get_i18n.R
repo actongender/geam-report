ec90aafefcac9511f7bdbc9221525e29ec3b1e51
Jörg Müller <joerg@toytic.com>
2020-10-26 18:30:16 +0100

    check language file not available

--- util/get_i18n.R
+++ util/get_i18n.R
@@ -9,22 +9,30 @@
 get_i18n <- function(key, plang=NULL){
 
         
+    # Default, use lsLangCode from config.R
     if (is.null(plang) & exists("lsLangCode")){
         plang <- lsLangCode 
         
-    # default is english    
+        # if no plang is specified and no lsLangCode available fall back to English
     }  else if (is.null(plang) & !exists("lsLangCode")){
         plang <- "en"
     }
     
+    
     plangfile <- paste0("i18n/geam-report-",plang,".csv")
     
-
+    # if language file is available
     if (file.exists(plangfile)){
+        
         i18n <- read_tsv(plangfile, 
                          col_types = cols())
+        
+    # otherwise use english
     } else {
-        warning("No language file for ", plang, " found. Using default.")
+        
+        i18n <- read_tsv(paste0("i18n/geam-report-en.csv"), 
+                         col_types = cols())
+        
     }
         
     txt <- i18n %>% 
