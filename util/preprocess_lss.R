43b442f3f2649d0e991c268e76ebf8f079176692
Jörg Müller <joerg@toytic.com>
2021-02-01 16:27:45 +0100

    fix lang preprocess

--- util/preprocess_lss.R
+++ util/preprocess_lss.R
@@ -208,12 +208,12 @@ if (length(isofrom) > 0){
     # retrieve the mapping for answer codes between languages. This constructs one map per
     # available language. For example, if the target language is "en" and submissions exist in 
     # "pl", "es", then there will be two mappings: "pl->en" and "es->en". 
-    if (exists("df.geam$SDEM012")){
+    if ("SDEM012" %in% colnames(df.geam)){
         mcs1 <- bind_rows(lapply(isofrom, map_iso3166, lsLangCode, alabels, id12[1]))
         df.geam$SDEM012 <- purrr::map2_chr(df.geam$SDEM012, df.geam$startlanguage, convert_iso3166, mcs1)
     }
     
-    if (exists("df.geam$SDEM013")){
+    if ("SDEM013" %in% colnames(df.geam)){
         mcs2 <- bind_rows(lapply(isofrom, map_iso3166, lsLangCode, alabels, id12[2]))
         df.geam$SDEM013 <- purrr::map2_chr(df.geam$SDEM013, df.geam$startlanguage, convert_iso3166, mcs2)
     }
